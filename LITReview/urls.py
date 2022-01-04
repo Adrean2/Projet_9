4a382895e922eeff58ba91d48554709c728672ce
Adrean <adrean337@gmail.com>
2022-01-04 18:28:10 +0100

    changed_landing_url

--- LITReview/urls.py
+++ LITReview/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('',include('authentication.urls')),
     path('admin/', admin.site.urls),
-    path('authentication/',include('authentication.urls')),
     path('blog/',include('blog.urls',))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
